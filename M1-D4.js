70179d0cd4e04413fa8236202a35112ac8fe3609
Abdul Samad Mahmood <72197815+ASMahmood@users.noreply.github.com>
2020-10-01 15:59:50 +0100

    Attempt_EX8_and_EX9

--- M1-D4.js
+++ M1-D4.js
@@ -122,13 +122,19 @@ console.log(reverseString("Strive"))
 Write a function "upperFirst" to capitalize the first letter of each word of a given string passed as parameter
 */
 
-/* WRITE YOUR CODE HERE */
+const upperFirst = function(S) {
+    S.split(" ")
+}
 
 /* EXERCISE 9
 Write a function "cutString" to create a new string without the first and last character of a given string.
 */
 
-/* WRITE YOUR CODE HERE */
+const cutString = function(S) {
+    return ((S.split("").shift()) + (S.split("").pop()));
+}
+
+console.log(cutString("Hello"))
 
 /* EXERCISE 10
 Write a function "giveMeRandom" which accepts a number n and returns an array containing n random numbers between 0 and 10
